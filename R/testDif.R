bca8b7f3807a9c21647d2ea58f9d9be50ffce074
jcanell4 <jcanell4@xtec.cat>
2024-07-19 01:31:53 +0200

    first commit

---
+++ R/testDif.R
@@ -0,0 +1,11 @@
+#' testDif
+#'
+#' This is a function to test the building of packages in R. This is a single
+#' function that return the difference of the parameters.
+#' @param n1 Is a number to substract the secont parameter
+#' @param n2 Is a number to sum to the first parameter
+#' @return A number with value equals to n1+n2.
+#' @export
+testDif<-function(n1, n2){
+  return (n1 - n2)
+}
---
+++ R/testSum.R
@@ -0,0 +1,11 @@
+#' testSum
+#'
+#' This is a function to test the building of packages in R. This is a single
+#' function that return the sum of the parameters.
+#' @param n1 Is a number to sum to teh secont parameter
+#' @param n2 Is a number to sum to the first parameter
+#' @return A number with value equals to n1+n2.
+#' @export
+testSum <- function(n1, n2) {
+  return (n1 + n2)
+}
